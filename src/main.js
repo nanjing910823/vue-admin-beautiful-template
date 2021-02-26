9ad2327bf64252c74a493dfd834d560cb385f4c6
Rankin <195960839@qq.com>
2021-02-26 17:51:21 +0800

    新增sunbimInstro

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import store from './store'
 import router from './router'
 import './plugins'
 import '@/layouts/export'
-/**
- * @author chuzhixin 1204505056@qq.com （不想保留author可删除）
- * @description 生产环境默认都使用mock，如果正式用于生产环境时，记得去掉
- */
+import './views/components/index'  //组件全局注册
 
 if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('@/utils/static')
---
+++ src/views/components/SunbimIntro/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-card>
+    <div slot="header">
+     <vab-icon :icon="['fas', 'info-circle']" style="color: #1890FF;"></vab-icon>
+      项目介绍
+      <el-button style="float: right; padding: 3px 0" type="text">详细信息>></el-button>
+    </div>
+    <div class="detail">
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">项目名称</el-tag></el-col>
+        <el-col :offset="1" :span="19"><span class="desc">中央商务区核心区配套学校（含邻里中心）</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">建设地址</el-tag></el-col>
+        <el-col :offset="1" :span="19"><span class="desc">南京市江北新区，东至横江大道，南至纵六路，西至横三路，北纵七路</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">建设单位</el-tag></el-col>
+        <el-col :offset="1" :span="19"><span class="desc">南京江北新区中央商务区开发运营有限公司</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">监理单位</el-tag></el-col>
+        <el-col :offset="1" :span="19"><span class="desc">南京江城工程管理有限公司</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">EPC单位</el-tag></el-col>
+        <el-col :offset="1" :span="19">
+          <ul style="list-style-type: none;padding-inline-start: 0px">
+            <li>江苏双楼建设集团有限公司</li>
+            <li>江苏省建筑设计研究院有限公司</li>
+          </ul>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4"><el-tag effect="plain" size="medium" class="header">施工日期</el-tag></el-col>
+        <el-col :offset="1" :span="19"><span class="desc">2020年4月-2021年4月15日</span></el-col>
+      </el-row>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'SunbimIntro'
+};
+</script>
+
+<style lang="scss">
+.detail {
+  div {
+    display: flex;
+    align-items: center;
+    padding: 3px 0px;
+    div {
+      .header {
+      }
+      .desc {
+      }
+    }
+  }
+}
+</style>
